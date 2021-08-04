89100613ac23b5e7c932ddb894c49350b699c837
polnops <polnops@gmail.com>
2021-08-03 17:14:13 -0700

    Add files via upload

---
+++ gaussian_fitfunctions.py
@@ -0,0 +1,68 @@
+import numpy as np
+from scipy import optimize
+
+
+def gaussian(height, center_x, center_y, width_x, width_y, rotation, offset):
+    """Returns a gaussian function with the given parameters"""
+    width_x = float(width_x)
+    width_y = float(width_y)
+    rotation = np.deg2rad(rotation)
+    center_xp = center_x * np.cos(rotation) - center_y * np.sin(rotation)
+    center_yp = center_x * np.sin(rotation) + center_y * np.cos(rotation)
+
+    def rotgauss(x, y):
+        xp = x * np.cos(rotation) - y * np.sin(rotation)
+        yp = x * np.sin(rotation) + y * np.cos(rotation)
+        g = height*np.exp(
+            -(((center_xp-xp)/width_x)**2 +
+                ((center_yp-yp)/width_y)**2)/2.) + offset
+        return g
+    return rotgauss
+
+
+def moments(data):
+    """Returns (height, x, y, width_x, width_y)
+    the gaussian parameters of a 2D distribution by calculating its
+    moments """
+    total = data.sum()
+    X, Y = np.indices(data.shape)
+    x = (X*data).sum()/total
+    y = (Y*data).sum()/total
+    sdat = np.sort(data.flatten())
+    offset = np.mean(data[:int(.01*len(sdat))])
+    if y < 0:
+        col = data[:, int(0)]
+    elif y > data.shape[1]:
+        col = data[:, int(data.shape[1]-1)]
+    else:
+        col = data[:, int(y)]
+    var_x = abs((np.arange(col.size)-y)**2*col).sum()/col.sum()
+    if var_x > 0:
+        width_x = np.sqrt(var_x)
+    else:
+        width_x = data.shape[0]
+    if x < 0:
+        row = data[int(0), :]
+    elif x > data.shape[0]:
+        row = data[int(data.shape[0]-1), :]
+    else:
+        row = data[int(x), :]
+    var_y = abs((np.arange(row.size)-x)**2*row).sum()/row.sum()
+    if var_y > 0:
+        width_y = np.sqrt(var_y)
+    else:
+        width_y = data.shape[1]
+    height = data.max()
+    return height, x, y, width_x, width_y, 0.0, offset
+
+
+def fitgaussian(data):
+    """Returns (height, x, y, width_x, width_y)
+    the gaussian parameters of a 2D distribution found by a fit"""
+    params = moments(data)
+
+    def errorfunction(p): return np.ravel(
+        gaussian(*p)(*np.indices(data.shape)) - data)
+    p, success = optimize.leastsq(errorfunction, params)
+    return p
+    return matrix, datum
---
+++ mode_overlap_functions.py
@@ -0,0 +1,173 @@
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import interpolate, signal
+from gaussian_fitfunctions import *
+
+
+def loadData(filename='normE_all.dat', param_name="w_slab"):
+
+    file = open(filename)
+    lst = []
+    for line in file:
+        lst += [line.split()]
+
+    param_list = [re.sub(r'\D', "", param) for param in lst[8]
+                  if param.startswith(param_name + "=")]
+
+    dat = convertStrtocomplex(lst)
+
+    return dat, param_list
+
+def convertStrtocomplex(lst):
+    b = np.char.replace(np.array(lst[9:]),'i','j')
+    return np.array(b,complex)
+
+def datToxyz(dat, i = 0):
+    return np.real(dat[:, 0]), np.real(dat[:, 1]), dat[:, 2+i]
+
+
+def cropData(x, y, z, bound, scale, res):
+
+    mask = (x < bound)*(x > -bound)*(y < bound)*(y > -bound)
+
+    x = x[mask]
+    y = y[mask]
+    z = z[mask]
+
+    # some of these lines below may be redundant for rectangular grid
+    corners = scale*np.array([min(y), max(y), min(x), max(x)])
+
+    grid_x, grid_y = np.mgrid[corners[0]:corners[1]:res,
+                              corners[2]:corners[3]:res]
+
+    values = interpolate.griddata((y, x), z, (grid_x, grid_y),
+                                  method='nearest')
+
+    return values, corners
+
+
+def plotData(dat, bound=4000, scale=1,
+             res=500j, plotflag=True, i=0):
+
+    x,y,z = datToxyz(dat,i)
+
+    values, corners = cropData(x, y, z, bound, scale, res)
+
+    if plotflag:
+        plt.imshow(values, origin='lower', extent=corners.tolist(),
+                   aspect='auto', cmap=plt.get_cmap('YlOrRd'))
+        plt.show()
+
+def plotComplexData(dat, param_list, bound=2000, scale=1, res=500j):
+             
+    """handles norm E, complex Ex and Ey, loops through param list"""
+
+    for i in range(len(param_list)): 
+        x,y,z = datToxyz(dat,i)
+
+        values, corners = cropData(x, y, z, bound, scale, res)    
+
+        if i%3 == 0:
+            print("w=",param_list[i])
+
+            fig, axs = plt.subplots(nrows=1, ncols=5, figsize=(10,2))
+            fig.suptitle('norm(E), Re(Ex), Im(Ex), Re(Ey), Im(Ey)')
+            axs[0].imshow(np.real(values), origin='lower', extent=corners.tolist(),
+                    aspect='auto', cmap=plt.get_cmap('YlOrRd'))
+  
+        elif i%3 == 1:
+            axs[1].imshow(np.real(values), origin='lower', extent=corners.tolist(),
+                    aspect='auto', cmap=plt.get_cmap('YlOrRd'))
+                
+            axs[2].imshow(np.imag(values), origin='lower', extent=corners.tolist(),
+                    aspect='auto', cmap=plt.get_cmap('YlOrRd'))
+
+            axs[1].set_yticklabels([])
+            axs[2].set_yticklabels([])
+            
+        elif i%3 == 2:
+            axs[3].imshow(np.real(values), origin='lower', extent=corners.tolist(),
+                    aspect='auto', cmap=plt.get_cmap('YlOrRd'))
+                
+            axs[4].imshow(np.imag(values), origin='lower', extent=corners.tolist(),
+                    aspect='auto', cmap=plt.get_cmap('YlOrRd'))
+
+            axs[3].set_yticklabels([])
+            axs[4].set_yticklabels([])
+                  
+                    
+
+def getCorrelation(values_ref, values):
+
+        xc = signal.correlate2d(
+            values_ref, values, mode="same", boundary='wrap')
+        xc_norm = xc**2/np.sum(values_ref**2)/np.sum(values**2)
+
+        return xc_norm
+
+
+def fitCorrelation(xc_norm):
+
+    gaussianfitparam = fitgaussian(xc_norm)
+    xcfit = gaussian(*gaussianfitparam)
+    normpeak = gaussianfitparam[0]
+
+    return normpeak, xcfit
+
+
+def plotCorrvsparams(param_list, xcpeaks):
+    param_list = np.array(param_list, dtype=float)
+    plt.plot(param_list, xcpeaks, marker='o', linestyle='')
+    plt.locator_params(axis="x", nbins=4)
+    plt.ylabel("peak correlation")
+    plt.xlabel("width (nm)")
+    plt.show()
+
+
+def plotCorr2D(xc_norm, xcfit, corners, fitflag):
+    plt.imshow(xc_norm, origin='lower', extent=corners.tolist(),
+                 aspect='auto', cmap=plt.get_cmap('YlOrRd'))
+    plt.colorbar()
+    if fitflag:
+        plt.contour(np.transpose(xcfit(*np.indices(xc_norm.shape))),
+                    origin='lower', extent=corners.tolist(), colors='w')
+
+    plt.show()
+
+
+def convolveData(dat_ref, dat_list, param_list,
+                 bound=2000, scale = 1,
+                 res=100j, plotflag1 = False,
+                 plotflag2=True, fitflag= False,
+                 i=0):
+
+    x = dat_ref[:, 0]
+    y = dat_ref[:, 1]
+    z = dat_ref[:, 2]
+    values_ref, corners_ref = cropData(x, y, z, bound,scale,res)     
+
+    xclist = []
+    xcpeaks = []
+    for i in range(len(param_list)):
+
+        x = dat_list[:, 0]
+        y = dat_list[:, 1]
+        z = dat_list[:, 2+i]
+
+        values, corners     = cropData(x, y, z, bound,scale,res)       
+        xc_norm = getCorrelation(values_ref, values)
+
+        if fitflag:
+            normpeak, xcfit = fitCorrelation(xc_norm)
+        else:
+            normpeak = np.max(xc_norm)
+
+        xclist.append(xc_norm)
+        xcpeaks.append(normpeak)
+
+        if plotflag1:
+            plotCorr2D(xc_norm, xcfit, corners, fitflag)
+
+    if plotflag2:
+        plotCorrvsparams(param_list, xcpeaks)
