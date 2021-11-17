dacc24d5cad9c8fac5ec740cc5053d30fe2e8f47
polnops <polnops@gmail.com>
2021-11-16 22:02:48 -0800

    fitting the width

--- waist_fit.py
+++ waist_fit.py
@@ -5,7 +5,7 @@ from complex_mode_overlap import *
 from commons import *
 
 
-def plotWaists(param_list, xcpeaks, xcpeaks2 =[]):
+def plotWaists(param_list, waists):
     param_list = np.array(param_list, dtype=float)
     plt.plot(param_list, waists, marker='o', linestyle='', label = "fit")
     plt.locator_params(axis="x", nbins=4)
@@ -17,23 +17,27 @@ def plotWaists(param_list, xcpeaks, xcpeaks2 =[]):
 def plotandfitNorm(dat, param_list, bound=2000, scale=1, res=500j):
 
     i = 0; waists = []
-    while i < len(param_list) and i%4 == 0:         
-  
+    while i < len(param_list):   
+
+
         x,y,z = datToxyz(dat,i)
         values, corners = cropData(x, y, z, bound, scale, res)    
       
-            norm2D = np.real(values)
+        norm2D = np.real(values)
 
-            plt.imshow(norm2D, origin='lower', extent=corners.tolist(),
-                    aspect='equal', cmap=plt.get_cmap('Greens'))
+        plt.figure()
+        plt.imshow(norm2D, origin='lower', extent=corners.tolist(),
+                aspect='equal', cmap=plt.get_cmap('Greens'))
 
-            waist, normfit = fitData(norm2D)
+        waist, normfit = fitData(norm2D)
 
-            waists.append(waist)
+        waists.append(8*waist*bound/np.abs(res))
 
-            plt.contour(np.transpose(normfit(*np.indices(normfit.shape))),
-                        origin='lower', extent=corners.tolist(), colors='w')
+        plt.contour(normfit(*np.indices(norm2D.shape)),
+            origin='lower', extent=corners.tolist(), colors='w')
 
-            plt.show()
+        plt.show()
 
-        i += 1
+        i += 4
+
+    return waists
