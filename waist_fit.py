b9ab0137657301699bde485636ef8f0f28b7a285
polnops <polnops@gmail.com>
2021-11-16 20:31:32 -0800

    wasit fit

--- waist_fit.py
+++ waist_fit.py
@@ -14,56 +14,22 @@ def plotWaists(param_list, xcpeaks, xcpeaks2 =[]):
     plt.xlabel("slab width (nm)")
     plt.show()
 
-def fitNorm(dat_ref, dat, param_list,
-                 bound=2000, scale = 1,
-                 res=100j, plotflag1 = False,
-                 plotflag2=True, fitflag= False,
-                 i=0):
-
-    param_list = np.unique(np.array(param_list,dtype=float))
-
-    xref, yref, zref = dattoxyComplex(dat_ref, 1)
-    values_ref, corners_ref = cropData(xref, yref, zref, bound,scale,res)   
-    #values dimension: (res, res, 1, 3)
-
-    n_param = len(param_list); xclist = []; xcpeaks1 = []; xcpeaks2 = []
-
-    x, y, z   = dattoxyComplex(dat, n_param)
-    values, corners = cropData(x, y, z, bound, scale, res)
-    #values dimension: (res, res, n_param, 3)
-
-    for i in range(n_param):
-               
-        xc_norm = getComplexCorrelation(values_ref, values, i)
-
-        normpeak1, xcfit = fitCorrelation(xc_norm)
-        
-        xclist.append(xc_norm)
-        xcpeaks1.append(normpeak1)
-        xcpeaks2.append(np.max(xc_norm))
-
-        if plotflag1:
-            plotCorr2D(xc_norm, corners, fitflag, xcfit = xcfit)
-
-    if plotflag2:
-        plotCorrvsparams(param_list, xcpeaks1, xcpeaks2)
-        return param_list, xcpeaks1, xcpeaks2
-
-
 def plotandfitNorm(dat, param_list, bound=2000, scale=1, res=500j):
 
-    i = 0
-
+    i = 0; waists = []
     while i < len(param_list) and i%4 == 0:         
   
         x,y,z = datToxyz(dat,i)
-
         values, corners = cropData(x, y, z, bound, scale, res)    
       
+            norm2D = np.real(values)
 
-            plt.imshow(np.real(values), origin='lower', extent=corners.tolist(),
+            plt.imshow(norm2D, origin='lower', extent=corners.tolist(),
                     aspect='equal', cmap=plt.get_cmap('Greens'))
 
+            waist, normfit = fitData(norm2D)
+
+            waists.append(waist)
 
             plt.contour(np.transpose(normfit(*np.indices(normfit.shape))),
                         origin='lower', extent=corners.tolist(), colors='w')
